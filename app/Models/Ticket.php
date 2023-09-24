6a8877a01a5c480dc80504de65dbbb7fe8ad7c16
Konstantin Bazhev <k.bazhev@sitesoft.ru>
2023-09-24 14:48:20 +0500

    init and create schema

---
+++ app/Models/Ticket.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Ticket extends Model
+{
+    use HasFactory;
+
+    /**
+     * @return HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function event(): HasOne
+    {
+        return $this->hasOne(Event::class, 'id', 'event_id');
+    }
+}
