69f78ee0c0777882bfd912406c76f75d09e4a3ad
Konstantin Bazhev <k.bazhev@sitesoft.ru>
2023-09-29 14:57:50 +0500

    fix: add rules

--- app/GraphQL/Mutations/Register.php
+++ app/GraphQL/Mutations/Register.php
@@ -12,17 +12,6 @@ final class Register
      */
     public function __invoke($_, array $args)
     {
-        $validator = \Validator::make($args, [
-            'name' => 'required|min:1',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8|confirmed',
-            'password_confirmation' => 'required|min:8',
-        ]);
-
-        if($validator->fails()){
-            throw new \Error('Unable to register');
-        }
-
         $args['password'] = \Hash::make($args['password']);
         $user = User::create($args);
         return $user;
--- app/GraphQL/Mutations/Upload.php
+++ app/GraphQL/Mutations/Upload.php
@@ -3,6 +3,7 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\Image;
+use App\Models\User;
 use Illuminate\Support\Facades\Storage;
 use PhpParser\Error;
 
@@ -14,14 +15,8 @@ final class Upload
      */
     public function __invoke($_, array $args)
     {
-        $validator = \Validator::make($args, [
-            'file' => 'required|image'
-        ]);
-
-        if($validator->fails()){
-            throw new Error('Invalid type');
-        }
-
+        /** @var User $user */
+        $user = \Auth::guard()->user();
 
         /** @var \Illuminate\Http\UploadedFile $file */
         $file = $args['file'];
@@ -31,6 +26,7 @@ final class Upload
         $image = new Image();
         $image->name = $file->getFilename();
         $image->hash = $file->hashName();
+        $image->user_id = $user->id;
         $image->save();
 
         return $image;
